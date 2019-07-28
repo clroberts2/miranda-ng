785dd2e9f5b30cebd25cfa1c6ce5b6f3823daad8
George Hazan <ghazan@miranda.im>
2019-07-28 23:23:21 +0300

    fixes installers packing problem

--- tools/build_scripts/z3_PackArchivesStable.bat
+++ tools/build_scripts/z3_PackArchivesStable.bat
@@ -9,14 +9,15 @@ for /F "tokens=1,2,3,4 delims= " %%i in (build\build.no.stable) do (
   )
 )
 
+call a_SetVar%tp%.bat
+
 if /i '%tp%' == '64' set bit=_x64
-if /i '%tp%' == '32' set CompileString=..\Tools\InnoSetup5\ISCC.exe /Dptx86 /DAppVer=%MirVer% "MirandaNG.iss" 
-if /i '%tp%' == '64' set CompileString=..\Tools\InnoSetup5\ISCC.exe /DAppVer=%MirVer% "MirandaNG.iss"
+if /i '%tp%' == '32' set CompileString=%ISCC% /Dptx86 /DAppVer=%MirVer% "MirandaNG.iss" 
+if /i '%tp%' == '64' set CompileString=%ISCC% /DAppVer=%MirVer% "MirandaNG.iss"
 
 set comp=%2
 if "%comp%"=="" (echo "please specify target compiler folder!" && pause && goto :EOF)
 
-call a_SetVar%tp%.bat
 if not exist %ArchDistr% mkdir %ArchDistr%
 
 pushd "%comp%\Symbols%tp%"
